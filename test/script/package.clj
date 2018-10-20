257dbf2c0d7b8ac149d28cf07e0d9c6e849f330a
aJchemist <valchemist@me.com>
2018-10-20 10:24:36 +0900

    Revised script.package

--- test/script/package.clj
+++ test/script/package.clj
@@ -4,17 +4,13 @@
    [clojure.tools.deps.alpha :as deps]
    [clojure.tools.deps.alpha.reader :as deps.reader]
    [clojure.tools.deps.alpha.extensions :as deps.ext]
-   [clojure.tools.deps.alpha.util.maven :as maven]
    [clojure.tools.deps.alpha.extensions.git :as deps.git]
-   [badigeon.maven.alpha :as badigeon.maven]
-   [badigeon.clean :as clean]
-   [badigeon.javac :as javac]
-   [badigeon.jar.maven.alpha :as jar]
-   [badigeon.install :as install]
-   [badigeon.sign :as sign]
-   [badigeon.deploy :as deploy]
-   [script.time]
    [user.java.io.alpha :as io]
+   [user.tools.deps.maven.alpha :as maven]
+   [user.tools.deps.clean :as clean]
+   [user.tools.deps.jar :as jar]
+   [user.tools.deps.install :as install]
+   [script.time]
    )
   (:import
    java.nio.file.Path
@@ -24,8 +20,8 @@
 (set! *warn-on-reflection* true)
 
 
-(def ^Path target-path (io/path "target"))
-(def ^Path classes-path (io/path-resolve target-path "classes"))
+(def target-path "target")
+(def classes-path "target/classes")
 (def ^Path library 'user.tools.deps.alpha)
 
 
@@ -36,8 +32,8 @@
       (clean/clean target-path)
       (let [version    (script.time/chrono-version-str)
             mvn-coords {:mvn/version version}
-            pom-file   (badigeon.maven/sync-pom library mvn-coords)
-            jarpath    (jar/jar library mvn-coords nil {:out-path (. target-path resolve "package.jar")})]
+            pom-file   (maven/sync-pom library mvn-coords)
+            jarpath    (jar/jar library mvn-coords nil {:out-path (. (io/as-path target-path) resolve "package.jar")})]
         (println (str (install/install library mvn-coords jarpath pom-file)))
         (println (str "\n- " version "\n"))
         jarpath))))
